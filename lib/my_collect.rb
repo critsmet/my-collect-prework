3eaeb2725032a1962ca14e9deffe8a6b59b153fe
Christopher Metzger <chrismetzgr@gmail.com>
2018-05-11 19:48:05 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,12 @@ def my_collect(something)
   i = 0
   capped = []
   while i < something.length
-  yield something[i]
+  capped << yield something[i]
   i += 1
 end
 return something
+end
+
+my_collect(something) do |x|
+  x
 end

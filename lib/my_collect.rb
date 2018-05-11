700abd08753dc2348da50155afbc94c10b025be6
Christopher Metzger <chrismetzgr@gmail.com>
2018-05-11 19:44:01 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(something)
   i = 0
   capped = []
   while i < something.length
-  yield capped.unshift(something[i]
+  yield capped.unshift(something[i])
   i += 1
 end
 return capped

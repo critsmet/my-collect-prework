97bd8d7b6a5f390fe4552531e755e1b47a4c23f8
Christopher Metzger <chrismetzgr@gmail.com>
2018-05-11 19:43:50 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,13 @@ def my_collect(something)
   i = 0
   capped = []
   while i < something.length
-  yield something[i]
+  yield capped.unshift(something[i]
+  i += 1
 end
+return capped
 end
 
 my_collect(something) do |x|
-  x.upcase
+  x.uppercase
+end
 end
